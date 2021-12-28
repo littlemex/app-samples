f74039c124aee15c180ebeee45cd0394d5cc0b6f
t-akzw <45358756+t-akzw@users.noreply.github.com>
2021-12-28 12:46:31 +0900

    method name add to Add

--- go-samples/api-sample/sample-v0.0.1/app/src/sample2/add_test.go
+++ go-samples/api-sample/sample-v0.0.1/app/src/sample2/add_test.go
@@ -20,7 +20,7 @@ func TestAdd(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := add(tt.args.a, tt.args.b); got != tt.want {
+			if got := Add(tt.args.a, tt.args.b); got != tt.want {
 				t.Errorf("add() = %v, want %v", got, tt.want)
 			}
 		})
